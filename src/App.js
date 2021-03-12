8eccd16c3c73d23de4532b923ad0d7addb81718a
himanshi-agrwal <himanshia@mindfiresolutions.com>
2021-03-12 14:36:05 +0530

    added signup/signin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Amplify from "aws-amplify";
+import logo from "./logo.svg";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    Amplify.configure({
+      Auth: {
+        region: process.env.REACT_APP_REGION,
+        userPoolId: process.env.REACT_APP_USER_POOL_ID,
+        userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+      },
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Profile Management</h1>
+        <h2>Authentication for Profile Management using AWS Cognito</h2>
       </header>
+      <SignUp />
+      <SignIn />
     </div>
   );
-}
+};
 
 export default App;
